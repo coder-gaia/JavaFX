86b50230980ab540d10f3ecdf569d2f56fb39e86
coder-gaia <alexandregaia911@gmail.com>
2025-01-14 19:50:00 -0300

    Sum operation

--- src/main/java/gui/ViewController.java
+++ src/main/java/gui/ViewController.java
@@ -4,14 +4,36 @@ import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.Locale;
 
 public class ViewController {
 
-        @FXML
-        public Button buttonTest;
+    @FXML
+    public TextField txtNumber1;
 
-        @FXML
-        public void onButtonTestAction(){
-            Alerts.showAlert("Alert title", null, "Worked", Alert.AlertType.ERROR);
-        }
+    @FXML
+    public TextField txtNumber2;
+
+    @FXML
+    public Label labelResult;
+
+    @FXML
+    public Button buttonSum;
+
+    @FXML
+    public void onButtonSumAction(){
+       try {
+           Locale.setDefault(Locale.UK);
+
+           double number1 = Double.parseDouble(txtNumber1.getText());
+           double number2 = Double.parseDouble(txtNumber2.getText());
+           double sum = number1 + number2;
+           labelResult.setText(String.format("%.2f", sum));
+       } catch (NumberFormatException e) {
+           Alerts.showAlert("Error", "Parse Error", e.getMessage(), Alert.AlertType.ERROR);
+       }
+    }
 }
